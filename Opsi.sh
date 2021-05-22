1ec4b8907afae8661f8c9986f66405b31666402b
TRI-ATMOKO <81742048+TRI-ATMOKO@users.noreply.github.com>
2021-05-22 23:53:30 +0700

    ColiTerus

---
+++ Opsi.sh
@@ -0,0 +1,73 @@
+clear
+k="\033[33;1m"
+ m="\033[31;1m"
+  pu="\033[39;1m"
+   p="\033[36;1m"
+    b="\033[34;1m"
+     h="\033[32;1m"
+      c="\033[35;1m"
+clear
+echo
+toilet -f big -F gay WELCOME TO
+echo
+toilet -f big -F gay SCRIPT 18+
+echo
+echo $m"SILAHKAN PILIH TOOLS NYA : "
+echo
+echo $c"1.) Link Bok*p part 1"
+echo $c"2.) Link Bok*p part 2"
+echo $c"3.) Link Bok*p part 3 (VPN ONlY)"
+echo $c"4.) Link Bok*p part 4"
+echo $b"5.) Follow Instagram Admin"
+echo $p"6.) Ga Jadi"
+echo $h
+read -p "Pilih : " Cuy
+
+if [ $Cuy = 1 ] || [ $Cuy = 1 ]
+then
+clear
+echo $h"	ANDA AKAN DI BAWA KE BROWSER"
+sleep 3
+xdg-open https://bit.ly/3wpKOzH
+fi
+
+if [ $Cuy = 2 ] || [ $Cuy = 2 ]
+then
+clear
+echo $h"	ANDA AKAN DI BAWA KE BROWSER"
+sleep 3
+xdg-open https://bit.ly/349IL6N
+fi
+
+if [ $Cuy = 3 ] || [ $Cuy = 3 ]
+then
+clear
+echo $h"	ANDA AKAN DI BAWA KE BROWSER"
+sleep 3
+xdg-open https://bit.ly/3454L2D
+fi
+
+if [ $Cuy = 4 ] || [ $Cuy = 4 ]
+then
+clear
+echo $h"	ANDA AKAN DI BAWA KE BROWSER"
+sleep 3
+xdg-open https://bit.ly/347bOrL
+fi
+
+if [ $Cuy = 5 ] || [ $Cuy = 5 ]
+then
+clear
+echo $h"	SEMOGA ANDA TERBEBAS DARI DOSA ANDA:v"
+sleep 3
+xdg-open https://instagram.com/eko_quotes07
+fi
+
+if [ $Cuy = 6 ] || [ $Cuy = 6 ]
+then
+clear
+echo $m"	SELAMAT TINGGAL PENDOSA:)"
+sleep 3
+exit
+fi
+
